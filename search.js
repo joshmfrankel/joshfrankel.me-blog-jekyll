5616fd57541784cc7ec23de699e6f83af9063521
Josh Frankel <joshmfrankel@gmail.com>
2015-06-03 17:28:48 -0400

    New blog post on minitest and guard with rails

---
+++ search.js
@@ -0,0 +1,17 @@
+---
+---
+var docs =
+[
+{% for post in site.posts limit:10 %}
+  {% include post.json %},
+{% endfor %}
+];
+// init lunr
+var idx = lunr(function () {
+  this.field('title', 10);
+  //this.field('content');
+})
+// add each document to be index
+for(var index in docs) {
+  idx.add(docs[index]);
+}
